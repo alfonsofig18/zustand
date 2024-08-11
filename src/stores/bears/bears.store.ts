9fa2e197b450ad9aeb33e848b334ecf7f8f80af4
Alfonso <alfonsofigueroa1515@gmail.com>
2024-08-11 13:39:48 -0500

    finalización o pausa del store Bears

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -10,6 +10,10 @@ interface BearState {
     polarBears: number;
     pandaBears: number;
     bears: Bear[];
+
+    computed: {
+        totalBears: number;
+    }
     //Methods
     increaseBlackBears: (num: number) => void;
     increasePolarBears: (num: number) => void;
@@ -20,18 +24,55 @@ interface BearState {
 
 }
 
-export const useBearStore = create<BearState>()((set) => ({
+export const useBearStore = create<BearState>()((set, get) => ({
     blackBears: 10,
     polarBears: 5,
     pandaBears: 1,
 
     bears: [{ id: 1, name: 'Alfo' }],
 
-    //methods
-    increaseBlackBears: (num: number) => set((state) => ({ blackBears: state.blackBears + num })),
-    increasePolarBears: (num: number) => set((state) => ({ polarBears: state.polarBears + num })),
-    increasePandaBears: (num: number) => set((state) => ({ pandaBears: state.pandaBears + num })),
+    computed: {
+        get totalBears() {
+            return get().blackBears + get().polarBears + get().pandaBears + get().bears.length;
+        }
+    },
 
-    doNothing: () => set((state) => ({ bears: [...state.bears] })),
-    
+    //methods
+    increaseBlackBears: (num: number) => set((state) => (
+        {
+            blackBears: state.blackBears + num
+        }
+    )),
+    increasePolarBears: (num: number) => set((state) => (
+        {
+            polarBears: state.polarBears + num
+        }
+    )),
+    increasePandaBears: (num: number) => set((state) => (
+        {
+            pandaBears: state.pandaBears + num
+        }
+    )),
+
+    doNothing: () => set((state) => (
+        {
+            bears: [...state.bears]
+        }
+    )),
+    addBears: () => set((state) => (
+        {
+            bears: [
+                ...state.bears,
+                {
+                    id: state.bears.length + 1,
+                    name: `Oso # ${state.bears.length + 1}`
+                }
+            ]
+        }
+    )),
+    clearBears: () => set(
+        {
+            bears: []
+        }
+    )
 }));
